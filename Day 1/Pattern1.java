7a8fc55677871df9a3129a687f6d6b073bc97d2f
Chandana Adusumalli <a.chandana1709@gmail.com>
2025-03-18 18:22:50 -0500

    DSA

--- Day 1/Pattern1.java
+++ Day 1/Pattern1.java
@@ -1,19 +1,21 @@
 // Rectangular Star Pattern
+//Time Complexity: n*n
+//Space Complexity: O(1)
+
 import java.util.Scanner;
 public class Pattern1{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int num = scanner.nextInt();
-        for (int i=0; i<num; i++){
-            for (int j=0; j<num; j++){
+        for (int i=0; i<num; i++){          // outer loop for rows
+            for (int j=0; j<num; j++){      // outer loop for columns
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println();           // To move to next line after printing each row
         }
         scanner.close();
     }
 }
 
-//Time Complexity: n*n
-//Space Complexity: 
+
